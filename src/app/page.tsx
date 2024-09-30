153334cedd1f71775e4732c3f5fad793b2366529
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-09-30 11:44:19 +0200

    updete page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import ActiveLabel from "./components/status-label";
+import NotActiveLabel from "./components/not-active-label";
+
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1 className="text-xl">Home Page</h1>
+      <ActiveLabel>Active</ActiveLabel>
+      <NotActiveLabel>Not Active</NotActiveLabel>
     </main>
   );
 }
