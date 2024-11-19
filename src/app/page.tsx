9e1de5f971d15f44bd9dcd18984f05c8743b6079
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-11-19 21:46:29 +0100

    dte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import AddCompanyButton from "./components/add-company-button";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <h1 className="text-xl">Home page </h1>
       <AddCompanyButton />
     </main>
   );
